05830013a8b4c417cf714f25c757fd27521e9875
Sidra Chaudhari <sidrasc05@gmail.com>
2025-06-26 19:06:26 +0530

    ...

---
+++ string.java
@@ -0,0 +1,9 @@
+class string {
+    static {
+        System.out.println("Static block");
+        System.exit(0);
+    }
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+}
